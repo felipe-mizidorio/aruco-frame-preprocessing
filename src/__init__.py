ec80bc43c91fcf4459a1217451a0a62a87d935da
felipe-mizidorio <felipemerenda1@gmail.com>
2026-04-20 16:10:15 +0100

    feat: add marker generation and saving functionality

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,5 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[logging.StreamHandler()]
+    handlers=[logging.StreamHandler()],
 )
---
+++ src/generate_markers.py
@@ -0,0 +1,32 @@
+import logging
+from pathlib import Path
+
+import cv2
+from cv2.typing import MatLike
+
+logger = logging.getLogger(__name__)
+
+
+def generate_marker(dictionary: int, marker_id: int, side_pixels: int) -> MatLike:
+    aruco_dictionary = cv2.aruco.getPredefinedDictionary(dictionary)
+    image = cv2.aruco.generateImageMarker(aruco_dictionary, marker_id, side_pixels)
+    return image
+
+
+def save_markers(
+    num_markers: int,
+    side_pixels: int,
+    output_dir: str,
+    dictionary: int = cv2.aruco.DICT_4X4_250,
+) -> None:
+    for i in range(num_markers):
+        image = generate_marker(dictionary, i, side_pixels)
+        cv2.imwrite(Path(output_dir + f"marker_{i}.png"), image)
+
+
+def main() -> None:
+    save_markers(10, 200, "data/raw")
+
+
+if __name__ == "__main__":
+    main()
